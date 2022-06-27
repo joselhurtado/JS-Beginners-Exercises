a0592b1482dd9992b6396f1bfbceee166ede471b
Jose Hurtado <hello@hurtadojose.com>
2022-06-27 15:38:31 +0000

    Exercise 22 done

--- exercises/22-The_Beatles/app.js
+++ exercises/22-The_Beatles/app.js
@@ -1,7 +1,16 @@
-// NEED HELP ON THIS
+// Code Already Compiled
 
 function sing() {
-    console.log("Let it be, let it be, let it be, let it be, whisper words of wisdom, let it be, let it be, let it be, let it be, let it be, there will be an answer, let it be");
+    let lyrics = "";
+    for(let i = 0; i < 4; i++){
+        lyrics += "let it be, " // Shortcut for add string
+    }
+    lyrics += "whisper words of wisdom, ";
+    for(let i = 0; i < 5; i++){
+        lyrics += "let it be, ";
+    }
+    lyrics += "there will be an answer, let it be";
+    return lyrics;
 }
 
 //Your code above ^^^
